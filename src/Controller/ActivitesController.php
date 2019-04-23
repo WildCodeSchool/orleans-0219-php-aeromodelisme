f15dde0087cd16343bd0363256033f50a543791c
Emilie SERRE <emilieserre@hotmail.com>
2019-04-23 17:22:58 +0200

    ajout de la page activités

---
+++ src/Controller/ActivitesController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class ActivitesController extends AbstractController
+{
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Activites/index.html.twig');
+    }
+}
