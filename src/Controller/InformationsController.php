cbf761b827476a556b0b613ec2fada0a64d5a681
Emilie SERRE <emilieserre@hotmail.com>
2019-04-24 12:13:03 +0200

    ajout de la page informations

---
+++ src/Controller/InformationsController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+class InformationsController extends AbstractController
+{
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Informations/index.html.twig');
+    }
+}
