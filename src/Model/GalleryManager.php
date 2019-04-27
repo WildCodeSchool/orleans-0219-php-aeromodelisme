d9206e125f01316f476b2f6d4508acf0596d3052
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-04-27 14:42:47 +0200

    Ajout d'une carde avec image en random

---
+++ src/Model/GalleryManager.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class GalleryManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'gallery';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+
+    public function randomPicture(): array
+    {
+        $pictures = $this->selectAll();
+        /*        return $this->pdo->query("SELECT * FROM $this->table
+                WHERE id=ROUND( RAND() *" . count($pictures) . " ) + 2")->fetch();*/
+        return $this->pdo->query("SELECT * FROM $this->table ORDER BY RAND() LIMIT 1")->fetch();
+    }
+}
